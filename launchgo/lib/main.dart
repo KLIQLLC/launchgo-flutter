0d8477bf898b7e3951bbc2ef8a0ec8444f861ea0
Igor Moisieiev <igor.moisieiev@gmail.com>
2025-08-18 19:21:11 -0400

    Splash screen changes. display white rocket

--- launchgo/lib/main.dart
+++ launchgo/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:launchgo/router/app_router.dart';
 import 'package:launchgo/services/auth_service.dart';
+import 'package:launchgo/widgets/splash_screen.dart';
 import 'package:provider/provider.dart';
 
 void main() {
@@ -21,23 +22,46 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   late final AppRouter _appRouter;
+  bool _showSplash = true;
 
   @override
   void initState() {
     super.initState();
     final authService = context.read<AuthService>();
     _appRouter = AppRouter(authService);
+    
+    // Show splash screen for 2 seconds
+    Future.delayed(const Duration(seconds: 2), () {
+      if (mounted) {
+        setState(() {
+          _showSplash = false;
+        });
+      }
+    });
   }
 
   @override
   Widget build(BuildContext context) {
+    if (_showSplash) {
+      return MaterialApp(
+        title: 'LaunchGo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFFFF5757)),
+          useMaterial3: true,
+        ),
+        home: const SplashScreen(),
+        debugShowCheckedModeBanner: false,
+      );
+    }
+
     return MaterialApp.router(
       title: 'LaunchGo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFFFF5757)),
         useMaterial3: true,
       ),
       routerConfig: _appRouter.router,
+      debugShowCheckedModeBanner: false,
     );
   }
 }
---
+++ launchgo/lib/widgets/splash_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Color(0xFFFE3732), // Red color from your gradient
+              Color(0xFFFF894B), // Orange color from your gradient
+            ],
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Try to load SVG, fallback to text
+              SvgPicture.asset(
+                'assets/images/launchgo_logo.svg',
+                height: 120,
+                width: 300,
+                fit: BoxFit.contain,
+                colorFilter: const ColorFilter.mode(
+                  Colors.white,
+                  BlendMode.srcIn,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
