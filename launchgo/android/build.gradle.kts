3fc04273ca400eca871404243949c66570e3a562
Igor Moisieiev <igor.moisieiev@gmail.com>
2025-09-15 13:17:25 -0400

    Add carashlytics

--- launchgo/android/build.gradle.kts
+++ launchgo/android/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
-    id("com.google.gms.google-services") version "4.4.0" apply false
+    id("com.google.gms.google-services") version "4.4.3" apply false
+    id("com.google.firebase.crashlytics") version "3.0.2" apply false
 }
 
 allprojects {
--- launchgo/lib/main.dart
+++ launchgo/lib/main.dart
@@ -1,3 +1,6 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:launchgo/config/environment.dart';
@@ -7,11 +10,25 @@ import 'package:launchgo/services/theme_service.dart';
 import 'package:launchgo/widgets/splash_screen.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   
   // Initialize environment configuration
   EnvironmentConfig.init();
+  
+  // Initialize Firebase
+  await Firebase.initializeApp();
+  
+  // Set up Crashlytics
+  FlutterError.onError = (errorDetails) {
+    FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+  };
+  
+  // Pass all uncaught asynchronous errors that aren't handled by the Flutter framework to Crashlytics
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
   
   // Lock orientation to portrait only
   SystemChrome.setPreferredOrientations([
