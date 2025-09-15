9a275f4cec24e3926bb86655a7456a89fd1c540a
Igor Moisieiev <igor.moisieiev@gmail.com>
2025-09-15 15:49:07 -0400

    Add proguard rules

--- launchgo/scripts/distribute_android.sh
+++ launchgo/scripts/distribute_android.sh
@@ -4,14 +4,14 @@
 
 echo "Building Android app..."
 
-# Build stage flavor with prod environment for demo
-flutter build apk --flavor stage --dart-define=ENV=prod
+# Build prod flavor with prod environment for demo
+flutter build apk --flavor prod --dart-define=ENV=prod
 
 echo "Distributing to Firebase App Distribution..."
 
-# Distribute stage build with prod env
-firebase appdistribution:distribute build/app/outputs/flutter-apk/app-stage-release.apk \
-  --app 1:481027521494:android:21d8229e2c967842240277 \
+# Distribute prod build with prod env
+firebase appdistribution:distribute build/app/outputs/flutter-apk/app-prod-release.apk \
+  --app 1:481027521494:android:212d21e1bc94b4cd240277 \
   --groups "testers" \
   --release-notes "${1:-Initial build}"
 
