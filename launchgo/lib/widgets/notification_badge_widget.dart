358efa4eed790a56f888b97c0446cd6a85bcfa6a
Igor Moisieiev <igor.moisieiev@gmail.com>
2025-10-19 21:53:42 -0400

    Notification badge changes. Display circle without number

--- launchgo/lib/widgets/notification_badge_widget.dart
+++ launchgo/lib/widgets/notification_badge_widget.dart
@@ -3,7 +3,6 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:provider/provider.dart';
 import '../services/notifications_api_service.dart';
 import '../services/theme_service.dart';
-import 'badge_icon.dart';
 
 /// Widget that displays notification alert icon with unread notification badge
 class NotificationBadgeWidget extends StatelessWidget {
@@ -26,18 +25,32 @@ class NotificationBadgeWidget extends StatelessWidget {
           child: IconButton(
             padding: const EdgeInsets.all(8.0),
             constraints: const BoxConstraints(),
-            icon: BadgeIcon(
-              icon: SvgPicture.asset(
-                'assets/icons/ic_alert.svg',
-                width: 20,
-                height: 20,
-                colorFilter: ColorFilter.mode(
-                  themeService.textColor,
-                  BlendMode.srcIn,
+            icon: Stack(
+              clipBehavior: Clip.none,
+              children: [
+                SvgPicture.asset(
+                  'assets/icons/ic_alert.svg',
+                  width: 20,
+                  height: 20,
+                  colorFilter: ColorFilter.mode(
+                    themeService.textColor,
+                    BlendMode.srcIn,
+                  ),
                 ),
-              ),
-              count: unreadCount,
-              showBadge: unreadCount > 0,
+                if (unreadCount > 0)
+                  Positioned(
+                    right: -8,
+                    top: -8,
+                    child: Container(
+                      width: 12,
+                      height: 12,
+                      decoration: const BoxDecoration(
+                        color: Color(0xFF6B2024),
+                        shape: BoxShape.circle,
+                      ),
+                    ),
+                  ),
+              ],
             ),
             onPressed: onPressed,
           ),
